2ec03223a9bce6fd5910adec272f35c396db73e7
Sameer Sahasrabuddhe <sameer@sbuddhe.net>
2020-07-27 20:03:47 +0530

    moving to the real domain; also added disqus

--- Pelican/publishconf.py
+++ Pelican/publishconf.py
@@ -10,8 +10,10 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://pelican.sbuddhe.net'
+SITEURL = 'https://sameer.sbuddhe.net'
 RELATIVE_URLS = False
+
+DISQUS_SITENAME = 'sameerds'
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
